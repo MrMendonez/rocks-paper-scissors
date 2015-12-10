22ea40fd8f45e892f90f02b40ff423225ccfb6c6
MrMendonez <dmendonez@gmail.com>
2015-12-09 20:55:59 -0500

    got modal working properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,13 @@
 // Scripts for Rocks Paper Scissors game
 
 $(document).ready(function(){
-  var choices = ["Rock", "Paper", "Scissors"];
+  // var choices = ["Rock", "Paper", "Scissors"];
   var round = 0;
   var userScore = 0;
   var computerScore = 0;
+  var tiedGameCount = 0;
 
   $(document).on('click', "#rock-button, #paper-button, #scissors-button", function(){
-
-    round++;
-    $("#round-number").html(round);
-    // if (round === 5){
-    //   alert("The game is over");
-    // }
 
     var computerChoice = Math.random();
     if (computerChoice < 0.34) {
@@ -31,6 +26,28 @@ $(document).ready(function(){
     $('#computers-choice').html(computerChoice);
     $('#users-choice').html(userChoice);
 
+    round++;
+    $("#round-number").html(round);
+    if (round === 5){
+      if (userScore > computerScore) {
+        $("#won-lost-or-tied-msg").html("You won!");
+      } else if (computerScore > userScore) {
+        $("#won-lost-or-tied-msg").html("You lost!");
+      } else {
+        $("#won-lost-or-tied-msg").html("Tied game!");
+      }
+      $("#game-over-modal").modal("show");
+      $("#game-over-modal").on('hidden.bs.modal', function (e) {
+        round = 0;
+        userScore = 0;
+        computerScore = 0;
+        tiedGameCount = 0;
+        $("#round-number").html(round);
+        $(".user-score").html(userScore);
+        $(".computer-score").html(computerScore);
+      })
+    }
+
   });
 
   var compare = function(choice1, choice2) {
@@ -43,6 +60,8 @@ $(document).ready(function(){
 
     if (choice1 === choice2) {
       $("#win-msg").html(tieMsg);
+      tiedGameCount++;
+      $("#tied-game-count").html(tiedGameCount);
       console.log(tieMsg);
       return tieMsg;
     } else if (choice1 === "Rock") {
@@ -50,13 +69,13 @@ $(document).ready(function(){
         $("#win-msg").html(rockWins);
         $("#you-win-or-lose-msg").html(youWin);
         userScore++;
-        $("#user-score").html(userScore);
+        $(".user-score").html(userScore);
         return rockWins;
       } else {
         $("#win-msg").html(paperWins);
         $("#you-win-or-lose-msg").html(youLose);
         computerScore++;
-        $("#computer-score").html(computerScore);
+        $(".computer-score").html(computerScore);
         return paperWins;   
       }
     } else if (choice1 === "Paper") {
@@ -64,13 +83,13 @@ $(document).ready(function(){
         $("#win-msg").html(paperWins);
         $("#you-win-or-lose-msg").html(youWin);
         userScore++;
-        $("#user-score").html(userScore);
+        $(".user-score").html(userScore);
         return paperWins; 
       } else {
         $("#win-msg").html(scissorsWins);
         $("#you-win-or-lose-msg").html(youLose);
         computerScore++;
-        $("#computer-score").html(computerScore);
+        $(".computer-score").html(computerScore);
         return scissorsWins;
       }
     } else {
@@ -79,13 +98,13 @@ $(document).ready(function(){
           $("#win-msg").html(scissorsWins);
           $("#you-win-or-lose-msg").html(youWin);
           userScore++;
-          $("#user-score").html(userScore);
+          $(".user-score").html(userScore);
           return scissorsWins;
         } else {
           $("#win-msg").html(rockWins);
           $("#you-win-or-lose-msg").html(youLose);
           computerScore++;
-          $("#computer-score").html(computerScore);
+          $(".computer-score").html(computerScore);
           return rockWins;
         }
       }
