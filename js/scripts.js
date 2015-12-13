ec97fff8b9d63a5660a84302546f81d5d0e43b70
MrMendonez <dmendonez@gmail.com>
2015-12-12 21:26:32 -0500

    unbinded controls during animation play and bind controls when animation ends.

--- js/scripts.js
+++ js/scripts.js
@@ -11,49 +11,17 @@ window.rps = {
 
 $(document).ready(function(){
   var choices = ["Rock", "Paper", "Scissors"];
+  var tieMsg = "Tied!";
+  var rockWins = "Rock wins!";
+  var paperWins = "Paper wins!";
+  var scissorsWins = "Scissors wins!";
+  var youLose = "You lose!"
+  var youWin = "You win!"
 
-
-
-
-
-  // Temporary while working with gameplay-panel
-  // $("#start-game").hide();
-  $("#gameplay-panel, #pause-msg, #round-screen").hide();
+  // Hide msgs
+  $("#gameplay-panel, #pause-screen, #round-screen").hide();
   $("#rock-screen, #paper-screen, #scissors-screen, #shoot-screen").hide();
-  $("#reveal-screen, #name-form").hide();
-  newButtonAnimation();
-
-
-  $("#new-button").on("click", function(){
-    $("#name-form").slideDown();
-    $("input").focus();
-  })
-
-  $("#submit-name-button").on("click", function(e) {
-    e.preventDefault();
-    var userName = $("input").val().trim();
-    bindControls();
-    $("#start-game").slideUp(1000, function() {
-      $("#panel-title").slideUp(1000);
-      $(".jumbotron").slideUp(1000, function() {
-        $("#gameplay-panel").slideDown(1000);
-      })
-    })
-  })
-
-  $("#start").on("click", function() {
-    if($(this).attr("data-status") === "on") {
-      $(this)
-        .attr("data-status", "off");
-      $(".btn-choices").off();
-      $("#pause-msg").slideDown();
-    } else {
-      $(this)
-        .attr("data-status", "on");
-      bindControls();
-      $("#pause-msg").slideUp();
-    }
-  })
+  $("#reveal-screen, #name-form, #end-of-round-screen").hide();
 
   function newButtonAnimation() {
     $("#new-button").on("mouseenter", function() {
@@ -62,6 +30,45 @@ $(document).ready(function(){
       $(this).toggleClass("btn-danger btn-success");
     })
   }
+
+  newButtonAnimation(); // Makes 'New' button change color on mouseover
+
+  $("#new-button").on("click", function(){
+    $("#name-form").slideDown(); // Input name form appears
+    $("input").focus();
+  })
+
+  $("#submit-name-button").on("click", function(e) {
+    e.preventDefault();
+    var userName = $("input").val().trim();
+    if (userName === "") {
+      userName = "Little Mac";
+    };
+    $(".username-placeholder").html(userName);
+    bindControls(); // Allow buttons to become active
+    // Hide Welcome screens and reveals gameplay panel
+    $("#start-game").slideUp(1000, function() {
+      $("#panel-title").slideUp(1000);
+      $(".jumbotron").slideUp(1000, function() {
+        $("#gameplay-panel").slideDown(1000);
+      })
+    })
+  })
+
+  // Pause game on start
+  $("#start").on("click", function() {
+    if($(this).attr("data-status") === "on") {
+      $(this)
+        .attr("data-status", "off");
+      $(".btn-choices").off();
+      $("#pause-screen").slideDown();
+    } else {
+      $(this)
+        .attr("data-status", "on");
+      bindControls();
+      $("#pause-screen").slideUp();
+    }
+  })
 
   function bindControls() {
     $(".btn-choices").on("click", function(){
@@ -75,6 +82,7 @@ $(document).ready(function(){
 
       $("#computers-choice").html(computerChoice);
       $("#users-choice").html(userChoice);
+
       function revealUserChoice() {
         if (userChoice === "Rock") {
           $("#reveal-screen-user-choice").addClass("fa-hand-" + userChoice.toLowerCase() + "-o").removeClass("fa-hand-paper-o fa-hand-scissors-o");
@@ -84,6 +92,7 @@ $(document).ready(function(){
           $("#reveal-screen-user-choice").addClass("fa-hand-" + userChoice.toLowerCase() + "-o").removeClass("fa-hand-rock-o fa-hand-paper-o");
         }
       };
+
       function revealComputerChoice() {
         if (computerChoice === "Rock") {
           $("#reveal-screen-computer-choice").addClass("fa-hand-" + computerChoice.toLowerCase() + "-o").removeClass("fa-hand-paper-o fa-hand-scissors-o");
@@ -93,17 +102,25 @@ $(document).ready(function(){
           $("#reveal-screen-computer-choice").addClass("fa-hand-" + computerChoice.toLowerCase() + "-o").removeClass("fa-hand-rock-o fa-hand-paper-o");
         }
       };
+
       revealUserChoice();
       revealComputerChoice();
 
       // Begin TV screen animations
-      $("#choose-screen").slideUp(1000, function() {
-        $("#round-screen").slideDown(1000).delay(500).hide(500, function() {
-          $("#rock-screen").slideDown(1000).delay(500).hide(500, function() {
-            $("#paper-screen").slideDown(1000).delay(500).hide(500, function() {
-              $("#scissors-screen").slideDown(1000).delay(500).hide(500, function() {
-                $("#shoot-screen").slideDown(1000).hide(function() {
-                  $("#reveal-screen").show();
+      $(".btn-choices").off();
+      $("#choose-screen").hide(500, function() {
+        $("#round-screen").slideDown(500).delay(500).hide(500, function() {
+          $("#rock-screen").show(500).delay(500).hide(500, function() {
+            $("#paper-screen").show(500).delay(500).hide(500, function() {
+              $("#scissors-screen").show(500).delay(500).hide(500, function() {
+                $("#shoot-screen").show(500).delay(500).hide(500, function() {
+                  $("#reveal-screen").fadeIn(1000).delay(500).fadeOut(500, function() {
+                    $("#end-of-round-screen").fadeIn(1000).delay(1000).fadeOut(500, function() {
+                      $("#choose-screen").slideDown(500, function() {
+                        bindControls(this); // Allows RPS buttons to bind.
+                      });
+                    }); 
+                  });
                 });
               })
             })
@@ -111,21 +128,15 @@ $(document).ready(function(){
         })
       });
 
-
-
-
-
-      
-
       rps.gameState.round++;
       $("#round-number").html(rps.gameState.round);
       if (rps.gameState.round === 5){
         if (rps.gameState.userScore > rps.gameState.computerScore) {
-          $("#won-lost-or-tied-msg").html("You won!");
+          $(".won-lost-or-tied-msg").html(youWin);
         } else if (rps.gameState.computerScore > rps.gameState.userScore) {
-          $("#won-lost-or-tied-msg").html("You lost!");
+          $(".won-lost-or-tied-msg").html(youLose);
         } else {
-          $("#won-lost-or-tied-msg").html("Tied game!");
+          $(".won-lost-or-tied-msg").html(tieMsg);
         }
         $("#game-over-modal").modal("show");
         $("#game-over-modal").on("hidden.bs.modal", function (e) {
@@ -140,42 +151,37 @@ $(document).ready(function(){
         })
       }
     })
-  };
+  }; // End bindControls function
 
   var compare = function(choice1, choice2) {
-    var tieMsg = "Tied!";
-    var rockWins = "Rock wins!";
-    var paperWins = "Paper wins!";
-    var scissorsWins = "Scissors wins!";
-    var youLose = "You lose this round!"
-    var youWin = "You win this round!"
 
     if (choice1 === choice2) {
       rps.gameState.tiedGameCount++;
       $("#win-msg").html(tieMsg);
       $(".tied-game-count").html(rps.gameState.tiedGameCount);
+      $(".won-lost-or-tied-msg").html(tieMsg);
     } else if (choice1 === "Rock") {
       if (choice2 === "Scissors") {
         $("#win-msg").html(rockWins);
-        $("#you-win-or-lose-msg").html(youWin);
+        $(".won-lost-or-tied-msg").html(youWin);
         rps.gameState.userScore++;
         $(".user-score").html(rps.gameState.userScore);
       } else {
         $("#win-msg").html(paperWins);
-        $("#you-win-or-lose-msg").html(youLose);
+        $(".won-lost-or-tied-msg").html(youLose);
         rps.gameState.computerScore++;
         $(".computer-score").html(rps.gameState.computerScore);
       }
     } else if (choice1 === "Paper") {
       if (choice2 === "Rock") {
         $("#win-msg").html(paperWins);
-        $("#you-win-or-lose-msg").html(youWin);
+        $(".won-lost-or-tied-msg").html(youWin);
         rps.gameState.userScore++;
         $(".user-score").html(rps.gameState.userScore);
         // r
       } else {
         $("#win-msg").html(scissorsWins);
-        $("#you-win-or-lose-msg").html(youLose);
+        $(".won-lost-or-tied-msg").html(youLose);
         rps.gameState.computerScore++;
         $(".computer-score").html(rps.gameState.computerScore);
       }
@@ -183,12 +189,12 @@ $(document).ready(function(){
       if (choice1 === "Scissors") {
         if (choice2 === "Paper") {
           $("#win-msg").html(scissorsWins);
-          $("#you-win-or-lose-msg").html(youWin);
+          $(".won-lost-or-tied-msg").html(youWin);
           rps.gameState.userScore++;
           $(".user-score").html(rps.gameState.userScore);
         } else {
           $("#win-msg").html(rockWins);
-          $("#you-win-or-lose-msg").html(youLose);
+          $(".won-lost-or-tied-msg").html(youLose);
           rps.gameState.computerScore++;
           $(".computer-score").html(rps.gameState.computerScore);
         }
@@ -198,4 +204,5 @@ $(document).ready(function(){
 });
 
 // To Do List:
-// When round = 0, 0 should not display on game. It should display as 'Round :' instead of 'Round: 0'. That's not how games behave.
+// Figure out how to pause animations if someone clicks start in the middle of the animations.
+// Unbind controls during animation play
